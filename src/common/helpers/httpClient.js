1cef0fa9affa4106555c96d63dc605cc2196cc2f
wilmer Martinez <wilmerm@conetweb.com>
2025-02-02 13:38:55 -0500

    Proyecto Piscinas primer commit

---
+++ src/common/helpers/httpClient.js
@@ -0,0 +1,42 @@
+import axios from 'axios';
+const apiUrl = import.meta.env.VITE_API_URL;
+
+const ErrorCodeMessages = {
+	401: 'Invalid credentials',
+	403: 'Access Forbidden',
+	404: 'Resource or page not found 3',
+};
+
+function HttpClient() {
+	const _errorHandler = (error) =>
+		Promise.reject(
+			Object.keys(ErrorCodeMessages).includes(error?.response?.status)
+				? ErrorCodeMessages[error.response.status]
+				: error.response.data && error.response.data.message
+				  ? error.response.data.message
+				  : error.message || error
+		);
+
+	const _httpClient = axios.create({
+		baseURL: apiUrl,
+		timeout: 6000,
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	});
+
+	_httpClient.interceptors.response.use((response) => {
+		return response.data;
+	}, _errorHandler);
+
+	return {
+		get: (url, config = {}) => _httpClient.get(url, config),
+		post: (url, data, config = {}) => _httpClient.post(url, data, config),		
+		patch: (url, config = {}) => _httpClient.patch(url, config),
+		put: (url, config = {}) => _httpClient.put(url, config),
+		delete: (url, config = {}) => _httpClient.delete(url, config),
+		client: _httpClient,
+	};
+}
+
+export default HttpClient();
