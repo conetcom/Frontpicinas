4b30e3d9b5e857263c89c0dcbfb8aa159162af75
wilmer Martinez <wilmerm@conetweb.com>
2025-02-02 22:11:40 -0500

    modificacion dashboard

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,20 +1,21 @@
 import { lazy } from 'react';
 import { Outlet, Route, Routes } from 'react-router-dom';
 
-//const Ecommerce = lazy(() => import('./Ecommerce'));
-//const Analytics = lazy(() => import('./Analytics'));
-const Project = lazy(() => import('./Project'));
+const Ecommerce = lazy(() => import('./Ecommerce'));
+const Analytics = lazy(() => import('./Analytics'));
+//const Project = lazy(() => import('./Project'));
 const CRM = lazy(() => import('./CRM'));
 const EWallet = lazy(() => import('./E-Wallet'));
+//<Route path="project" element={<Project />} />
 
 export default function Dashboard() {
 	return (
 		<Routes>
 			<Route path="/*" element={<Outlet />}>
 				<Route index element={<Ecommerce />} />
-				//<Route path="analytics" element={<Analytics />} />
-				//<Route path="ecommerce" element={<Ecommerce />} />
-				<Route path="project" element={<Project />} />
+				<Route path="analytics" element={<Analytics />} />
+				<Route path="ecommerce" element={<Ecommerce />} />
+				
 				<Route path="crm" element={<CRM />} />
 				<Route path="e-wallet" element={<EWallet />} />
 			</Route>
