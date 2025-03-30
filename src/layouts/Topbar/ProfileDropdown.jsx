e7d2875179e50261ad3e75ebf3a51ab96924a2ac
wilmer Martinez <wilmerm@conetweb.com>
2025-03-30 11:27:42 -0500

    modificacion profile dropdown inicial y userbox inicial

--- src/layouts/Topbar/ProfileDropdown.jsx
+++ src/layouts/Topbar/ProfileDropdown.jsx
@@ -1,24 +1,11 @@
-import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Dropdown } from 'react-bootstrap';
-import { useAuthContext } from '@/common/context';
 import classNames from 'classnames';
 import { useToggle } from '@/hooks';
 
-const ProfileDropdown = ({ userTitle, username, menuItems, userImage}) => {
-	const { user} = useAuthContext();
-	// Estado para la imagen de perfil, inicializando con userImage o una predeterminada
-	const [profileImage, setProfileImage] = useState(user?.profileImage || userImage);
-	
-	const [isOpen, toggleDropdown] = useToggle();
 
-	// Este useEffect detecta si la imagen del perfil en el contexto del usuario cambia
-	useEffect(() => {
-		// Si la imagen de perfil en el objeto user está definida, actualizamos el estado con la nueva imagen
-		if (user?.profileImage) {
-			setProfileImage(user?.profileImage);
-		}
-	}, [user?.profileImage, userImage]);
+const ProfileDropdown = ({ userTitle, username, menuItems, userImage }) => {
+	const [isOpen, toggleDropdown] = useToggle();
 
 	return (
 		<Dropdown show={isOpen} onToggle={toggleDropdown}>
@@ -30,8 +17,7 @@ const ProfileDropdown = ({ userTitle, username, menuItems, userImage}) => {
 				className="nav-link dropdown-toggle arrow-none nav-user px-2"
 			>
 				<span className="account-user-avatar">
-					{/* Usamos el estado profileImage para mostrar la imagen actualizada */}
-					<img src={profileImage} className="rounded-circle" width={32} alt="user" />
+					<img src={userImage} className="rounded-circle" width={32} alt="user" />
 				</span>
 				<span className="d-lg-flex flex-column gap-1 d-none">
 					<h5 className="my-0">{username}</h5>
@@ -43,16 +29,18 @@ const ProfileDropdown = ({ userTitle, username, menuItems, userImage}) => {
 					<div className="dropdown-header noti-title">
 						<h6 className="text-overflow m-0">Welcome !</h6>
 					</div>
-					{menuItems.map((item, i) => (
-						<Link
-							to={item.redirectTo}
-							className="dropdown-item notify-item"
-							key={i + '-profile-menu'}
-						>
-							<i className={classNames(item.icon, 'me-1')}></i>
-							<span>{item.label}</span>
-						</Link>
-					))}
+					{menuItems.map((item, i) => {
+						return (
+							<Link
+								to={item.redirectTo}
+								className="dropdown-item notify-item"
+								key={i + '-profile-menu'}
+							>
+								<i className={classNames(item.icon, 'me-1')}></i>
+								<span>{item.label}</span>
+							</Link>
+						);
+					})}
 				</div>
 			</Dropdown.Menu>
 		</Dropdown>
