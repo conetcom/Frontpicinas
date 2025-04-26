ab90a4d3890108343a8026bd4f9a86284aba0411
wilmer Martinez <wilmerm@conetweb.com>
2025-04-25 19:43:35 -0500

    Remove package-lock.json to avoid conflicts with yarn

--- src/common/context/index.js
+++ src/common/context/index.js
@@ -5,7 +5,7 @@ export * from './useNotificationContext'; */
 // src/common/context/index.js
 
 export { useAuthContext, AuthProvider } from './useAuthContext';
-
+export { useNotificationContext } from './useNotificationContext';
 export * from './useThemeContext';
 // Agrega más si tienes otros contextos
 export { NotificationProvider, useNotifications } from './useNotificationContext';
